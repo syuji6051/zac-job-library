766763418be5f972ba44fb40823c87da3d140e1b
syuji <syuji.okamura@gmail.com>
2021-04-25 07:16:08 +0900

    error message

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -3,13 +3,12 @@ import { NextFunction, Request, Response } from 'express';
 function errorHandler() {
   return (error: any, _request: Request, response: Response, next: NextFunction) => {
     const status = error.status || 500;
-
     if (error) {
       response
         .status(status)
         .send({
           code: 'server_error',
-          message: (typeof error === 'object') ? JSON.parse(error) : error,
+          message: error.message,
         });
     } else {
       next(error);
