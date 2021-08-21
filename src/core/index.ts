95cf5ab37d62ab8506675dae30f4c621aa3a79c8
syuji <syuji.okamura@gmail.com>
2021-08-21 14:46:22 +0900

    add s3 instance

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,6 +1,6 @@
 export * from './ajv';
 export * from './asm';
-export * from './s3';
+export * as s3 from './s3';
 export * as errors from './errors';
 export { default as logger } from './logger';
 export * from './crypto';
