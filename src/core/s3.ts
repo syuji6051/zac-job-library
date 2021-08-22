e38a75c57d4dbdd7a77d4f90ef934ab1b69ea211
syuji <syuji.okamura@gmail.com>
2021-08-22 09:58:21 +0900

    add s3 instance

--- src/core/s3.ts
+++ src/core/s3.ts
@@ -1,6 +1,8 @@
 import aws from 'aws-sdk';
 
-const s3instance = new aws.S3();
+const s3instance = new aws.S3({
+  region: process.env.AWS_REGION,
+});
 
 export {
   // eslint-disable-next-line import/prefer-default-export
