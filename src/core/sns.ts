6b0fa5cb2d71c42c31a4c07dc2319eb4b0551544
syuji <syuji.okamura@gmail.com>
2021-12-29 07:33:25 +0900

    add publish

--- src/core/sns.ts
+++ src/core/sns.ts
@@ -1,8 +1,23 @@
 import aws from 'aws-sdk';
 
+const { STAGE } = process.env;
+
 const snsInstance = new aws.SNS();
 
+const publish = async <T>(data: T, topicArn: string) => {
+  await snsInstance.publish({
+    TopicArn: topicArn,
+    Message: JSON.stringify(data),
+    MessageAttributes: {
+      stage: {
+        DataType: 'String',
+        StringValue: STAGE,
+      },
+    },
+  }).promise();
+};
+
 export {
-  // eslint-disable-next-line import/prefer-default-export
   snsInstance,
+  publish,
 };
