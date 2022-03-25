2b9a01f6a7f7dc892e887b115d073120b748d7dd
syuji <syuji.okamura@gmail.com>
2022-03-26 08:03:24 +0900

    slack

--- src/services/sns.ts
+++ src/services/sns.ts
@@ -1,7 +1,7 @@
-import { slack } from '@syuji6051/zac-job-interface';
 import { publish } from '../core/sns';
+import { PublishSnsToSlackMessage } from '../../interface/src/slack';
 
 // eslint-disable-next-line import/prefer-default-export
-export const publishSnsToSlack = async (topic: string, message: slack.PublishSnsToSlackMessage) => {
+export const publishSnsToSlack = async (topic: string, message: PublishSnsToSlackMessage) => {
   await publish(message, topic);
 };
